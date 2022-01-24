cd736230282fa0bf15d35b9ce19828d91d5aa37f
AbderrahimAgra <abder.agra@gmail.com>
2022-01-24 08:47:44 +0100

    html to php

--- dashbord.php
+++ dashbord.php
@@ -18,59 +18,7 @@
     <div class="container-fluid">
       <div class="row">
         <!-- sidebar -->
-        <div class="sidebar col-md-3 col-2 vh-100 d-flex flex-column align-items-center pt-3">
-          <h1 class="text-start w-100 border-start border-info border-5  ps-md-2 d-none d-md-block h1">E-classe </h1>
-          <h1 class="text-start w-100 border-start border-info border-5  ps-md-2 d-md-none h5">E-classe </h1>
-          <!-- img__logo -->
-          <img src="images/logo.jfif" class="rounded-circle w-50 my-4" alt="logo">
-          <h5 class="d-none d-md-block"> Admin name</h5>
-          <p class="text-info ">Admin</p>
-          <!-- list-unstyled -->
-          <ul class="list-unstyled w-md-75 btn d-flex flex-column gap-2">
-              <li>
-                <a href="#" class="btn btn-info w-100 text-dark  ">
-                  <img class="icons" src="images/ic-home.svg" alt="logo">
-                  <span class="w-50 d-none d-md-inline-block">Home</span>
-                </a>
-              </li>
-              <li>
-                <a href="" class="btn">
-                  <img src="images/cources.svg" alt="logo" class="icons">
-                  <span class="w-50 d-none d-md-inline-block">Course</span>
-                </a>
-              </li>
-              <li>
-                <a href="http://127.0.0.1:5500/students.html" class="btn">
-                  <img src="images/student.svg" alt="logo" class="icons">
-                  <span class="w-50 d-none d-md-inline-block">Students</span>
-                </a>
-              </li>
-              <li>
-                <a href="http://127.0.0.1:5500/payment.html" class="btn">
-                  <img src="images/payment.svg" alt="logo" class="icons">
-                  <span class="w-50 d-none d-md-inline-block">Payement</span>
-                </a>
-              </li>
-              <li>
-                <a href="" class="btn">
-                  <img src="images/report.svg" alt="logo" class="icons">
-                  <span class="w-50 d-none d-md-inline-block">raport</span>
-                </a>
-              </li>
-              <li>
-                <a href="" class="btn mb-md-5">
-                  <i class="bi bi-sliders"></i>
-                  <span class="w-50 d-none d-md-inline-block">setting</span>
-                </a>
-              </li>
-              <li class=" w-100 mt-md-5">
-                <a href="" class="btn">
-                  <span class="w-50 d-none d-md-inline-block">Logout</span>
-                  <img src="images/logout.svg" alt="logo" class="icons">
-                </a>
-              </li>
-          </ul>
-        </div>
+       <?php include("sidebare.php"); ?>
         <!-- zone de la droite-->
         <div class="col-10 col-md-9 ">
           <div class="row navbar-con">
