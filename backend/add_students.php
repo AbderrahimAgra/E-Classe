5e838c0ba7bf9d8bc7182c71b70e340c0cbe8ed8
AbderrahimAgra <abder.agra@gmail.com>
2022-03-16 14:40:45 +0100

    editer sur les type des inpute

--- backend/add_students.php
+++ backend/add_students.php
@@ -24,23 +24,23 @@ if(!isset($_SESSION['user'])){
   <div class="col-md-8">
   <form action="add.php" method="POST" >
    <div>
-      <label for="exampleInputEmail1">Name</label>
+      <label for="#">Name</label>
       <input type="text" class="form-control" id="name" name="name" placeholder="Enter name">
     </div>
     <div class="form-group">
-      <label for="exampleInputEmail1">Email</label>
+      <label for="#">Email</label>
       <input type="email" class="form-control" id="email" name="email" placeholder="Enter email">
     </div>
     <div class="form-group">
-      <label for="exampleInputEmail1">Phone</label>
+      <label for="#">Phone</label>
       <input type="number" class="form-control" id="phone" name="phone" placeholder="Enter number phone">
     </div>
     <div class="form-group">
-      <label for="exampleInputEmail1">Enroll Number</label>
-      <input type="enumber" class="form-control" id="enroll_number" name="enroll_number" >
+      <label for="#">Enroll Number</label>
+      <input type="number" class="form-control" id="enroll_number" name="enroll_number" >
     </div>
     <div class="form-group">
-      <label for="exampleInputEmail1">Date of admission</label>
+      <label for="#">Date of admission</label>
       <input type="date" class="form-control" id="date" name="date" >
     </div>
     <div class="">
