f01c837ac474cfd6c4a6e36511eeaf20019aca72
AbderrahimAgra <abder.agra@gmail.com>
2022-03-16 14:48:09 +0100

    modifier les variable a fin d'avoire des error au niveau de variable  $password inclouder par la connection et le second exister dans la page

--- backend/connection.php
+++ backend/connection.php
@@ -1,10 +1,10 @@
 <?php
- $servername = 'localhost' ;
- $username = 'root' ;
- $password = '' ;
- $db = 'e-class' ;
+ $db_servername = 'localhost' ;
+ $db_username = 'root' ;
+ $db_password = '' ;
+ $db_name = 'e-class' ;
 //creat connection
- $connection = mysqli_connect( $servername , $username , $password , $db ) ;
+ $connection = mysqli_connect( $db_servername , $db_username , $db_password , $db_name ) ;
  //verefication
  if(!$connection){
    die ('connection failed' . mysqli_connect_error()) ;
